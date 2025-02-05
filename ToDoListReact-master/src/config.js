dccd13360569d41df5d9def9e6a9ea24615d330e
yaeli bloch <yael13269@gmail.com>
2025-02-05 20:44:45 +0200

    change url

---
+++ ToDoListReact-master/src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    apiUrl: process.env.REACT_APP_URL.trim() 
+  };
+  
+  export default config;
--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -5,27 +5,27 @@ const apiUrl = "http://localhost:5000"
 export default {
   
   getTasks: async () => {
-    const result1 = await axios.get(`${apiUrl}/api/tasks`)    
+    const result1 = await axios.get(`${config.apiUrl}/api/tasks`)    
     return result1.data;
   },
 
   addTask: async(name)=>{  
     const currentSecond= new Date().getSeconds();
     console.log('addTask', name)
-    const result= await axios.post(`${apiUrl}/api/tasks`,
+    const result= await axios.post(`${config.apiUrl}/api/tasks`,
       {Id:currentSecond,Name:name,IsComplete:false})
       return result.data;
   },
 
   setCompleted: async(id, isComplete)=>{
     console.log('setCompleted', {id, isComplete})
-    const result = await axios.put(`${apiUrl}/api/tasks/${id}`,{id,isComplete})    
+    const result = await axios.put(`${config.apiUrl}/api/tasks/${id}`,{id,isComplete})    
     return result.data;
   },
 
   deleteTask:async(id)=>{
     console.log('deleteTask')
-    const result= await axios.delete(`${apiUrl}/api/tasks/${id}`,id)
+    const result= await axios.delete(`${config.apiUrl}/api/tasks/${id}`,id)
     return result.data;
   }
 }
