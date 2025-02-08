33bbf875255f84fe748e87938d2d49cc22fd31d8
yaeli bloch <yael13269@gmail.com>
2025-02-08 21:06:24 +0200

    add node for render

---
+++ render_api_project/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+// Endpoint של GET שמחזיר את רשימת האפליקציות
+app.get('/apps', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.render.com/v1/services', {
+            headers: {
+                'Authorization': `Bearer rnd_wHQAOSawLsZD5AeQgbB8jB5MhJgv`
+            }
+        });
+        res.json(response.data);  // מחזיר את המידע שנמצא ב-Render
+    } catch (error) {
+        res.status(500).json({ error: 'error : in loading the data '});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
