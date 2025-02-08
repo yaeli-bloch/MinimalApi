88f5ff2d05693c4ecf3ba7404327ef80b5468b77
yaeli bloch <yael13269@gmail.com>
2025-02-09 01:45:11 +0200

    port

--- render_api_project/server.js
+++ render_api_project/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-// Endpoint של GET שמחזיר את רשימת האפליקציות
+
 app.get('/apps', async (req, res) => {
     try {
         const response = await axios.get('https://api.render.com/v1/services', {
